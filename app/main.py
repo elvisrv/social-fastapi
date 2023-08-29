55b08ac1e50ce1a729c108c05b0bf23e85fe7a4f
Elvis <elvisrv@gmail.com>
2023-08-28 23:27:33 -0300

    packages adjustments

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from models import Base
+import models as m
 from database import engine
 from routers import post, user, auth, vote
 from config import settings
 
-Base.metadata.create_all(bind=engine)
+m.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
